459a1da03b4cf919b8806c37658e0ecd84f62f06
Daniel Omole <danielomole497@yahoo.com>
2024-06-24 21:51:13 -0400

    splashscreen

--- src/screens/auth/SplashScreen.tsx
+++ src/screens/auth/SplashScreen.tsx
@@ -1,11 +1,26 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useColorScheme } from 'nativewind'
+import Animated, { FadeInRight } from "react-native-reanimated"
+import{ Image } from "expo-image";
+import { NavigationProp, useNavigation } from '@react-navigation/native'
 
 const SplashScreen = () => {
+  const { colorScheme, toggleColorScheme } = useColorScheme()
+  const { navigate }: NavigationProp<SplashNavigationType> = useNavigation();
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //  navigate("Welcome");
+    // }, 2000);
+  }, []);
+
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>SplashScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
